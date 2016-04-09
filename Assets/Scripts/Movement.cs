d00edf53c3c1063726609c6fd11c097d9abf39dc
a.bazhanau <alex.bazhanau@adform.com>
2016-04-09 20:57:23 +0300

    Movement added
    # Conflicts:
    #	Assets/Scenes/main.unity
    #	ProjectSettings/ProjectSettings.asset

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float speedMultiplyer;
+
+    private Rigidbody rigidbody;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    var transform = GetComponentInParent<Transform>();
+	    rigidbody = GetComponent<Rigidbody>();
+	}
+	
+    void FixedUpdate()
+    {
+        float input = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(transform.forward * input * speedMultiplyer);
+    }
+}
