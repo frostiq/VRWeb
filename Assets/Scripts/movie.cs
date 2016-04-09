7f8708743fc1a6eec05699d71193696027d9b2ae
a.bazhanau <alex.bazhanau@adform.com>
2016-04-09 16:13:35 +0300

    basic example added

---
+++ Assets/Scripts/movie.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movie : MonoBehaviour {
+    public MovieTexture movTexture;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
