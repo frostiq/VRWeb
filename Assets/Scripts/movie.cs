193cf4dadd7114f6ba1cb0b55adad9b29b0cf9b3
a.bazhanau <alex.bazhanau@adform.com>
2016-04-10 12:56:02 +0300

    second sphere fixed
    movie loop fixed

--- Assets/Scripts/movie.cs
+++ Assets/Scripts/movie.cs
@@ -6,6 +6,7 @@ public class movie : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         GetComponent<Renderer>().material.mainTexture = movTexture;
+	    movTexture.loop = true;
         movTexture.Play();
 	}
 	
